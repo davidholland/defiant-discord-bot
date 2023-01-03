999c595184c5f4d08157cc8f303acf4536720256
Holland <hldave@a483e75c50b1.ant.amazon.com>
2023-01-03 14:41:42 -0700

    add weak aaura

--- bot.py
+++ bot.py
@@ -116,11 +116,9 @@ With UserID %s
 
 Happy Tuesday Defiant!
 
-__Details for the week of %s/%s/%s__
-```
+__Details for the week of %s/%s/%s__```
 
-```
-The M+ affixes are...''' % (date_today.month, date_today.day, date_today.year)
+```The M+ affixes are...''' % (date_today.month, date_today.day, date_today.year)
         except Exception as e:
             print("A thing broke: %s" % e)
             return message
@@ -139,7 +137,7 @@ The M+ affixes are...''' % (date_today.month, date_today.day, date_today.year)
                     **%s** - %s
                     **%s** - %s
                     **%s** - %s
-                    **%s** - %s
+                    **%s** - %s | Weak Aura to track: https://wago.io/thundering-affix
                     ''' % (page_data['affix_details'][0]['name'], page_data['affix_details'][1]['name'], page_data['affix_details'][2]['name'], page_data['affix_details'][3]['name'],
                         page_data['affix_details'][0]['name'], page_data['affix_details'][0]['description'],
                         page_data['affix_details'][1]['name'], page_data['affix_details'][1]['description'],
