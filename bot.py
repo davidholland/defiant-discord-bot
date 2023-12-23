aa63f110f659676ef26902b49600d6de2cd16a90
David Holland <dave@dholland.net>
2023-12-22 21:27:02 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -290,7 +290,7 @@ lfg - Adds you to the wow-lfg channel if you are interested in notifications whe
             await send_message(channel=message.channel, message=table, send_file=None)
 
         elif message.content.lower().startswith('!dadjoke'):
-            dadejoke = dadjokes.Dadjoke()
+            dadjoke = dadjokes.Dadjoke()
             await send_message(channel=message.channel, message=dadjoke.joke, send_file=None)
 
         elif message.content.lower().startswith('!timer'):
