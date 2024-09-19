cc398e29d09b8168994c4794953b0c8d78e30875
David Holland <dave@dholland.net>
2024-09-19 09:58:02 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -138,7 +138,7 @@ __Details for the week of %s/%s/%s__```
                     +4  - **%s** - %s
                     +7  - **%s** - %s
                     +10 - **%s** - %s
-                    +12 - **Xal'atath's Guile** - All benefits from Xal'atath's Bargains are removed. All enemies have a flat 20% increase to their HP and damage
+                    +12 - **Xal'atath's Guile** - All benefits from Xal'atath's Bargains are removed. All enemies have a flat 20 percent increase to their HP and damage.
                     ''' % (page_data['affix_details'][0]['name'], page_data['affix_details'][1]['name'], page_data['affix_details'][2]['name'],page_data['affix_details'][3]['name'],
                         page_data['affix_details'][0]['name'], page_data['affix_details'][0]['description'],
                         page_data['affix_details'][1]['name'], page_data['affix_details'][1]['description'],
